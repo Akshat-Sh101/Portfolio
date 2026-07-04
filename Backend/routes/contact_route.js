45b056380fc3a7a965a77e8914ebe3ce4eb8cb05
Akshat Sharma <akshatsharmap42@gmail.com>
2026-07-04 22:16:19 +0530

    changes in transporter

--- Backend/routes/contact_route.js
+++ Backend/routes/contact_route.js
@@ -41,7 +41,9 @@ router.post("/", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.APP_PASSWORD,
